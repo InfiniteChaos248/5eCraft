6e335c2def54e987c5ed793a48657dd5ee7b337e
Arjun <svarjun1997@gmail.com>
2025-10-25 05:11:05 +0530

    vite chunk config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				}
+			}
+		}
+	}
 });
